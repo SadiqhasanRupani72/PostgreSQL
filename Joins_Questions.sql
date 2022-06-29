1c2e98d35a401a0d36a13943b5a71a453dc63a00
Sadiqhasan Rupani <sadiqhasanrupani11@gmail.com>
2022-06-29 22:51:51 +0530

    .added and .modifided

--- Joins_Questions.sql
+++ Joins_Questions.sql
@@ -1,7 +1,3 @@
--- California sales tax laws have changed and we need to alert our customers to this 
--- through email.
--- What are the emails of the customers who live in California?
-
 select * from address;
 
 select * from customer;
